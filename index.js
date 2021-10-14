5bf62fb71b7cd2638f1b4d08f9825d4b27f9a3c0
CloterChohan <CloterChohan@gmail.com>
2021-10-14 10:03:56 +0700

    Able to Run now

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const Discord = require('discord.js');
+const auth = require('./auth.json');
+// Initialize Discord Bot
 
+const bot = new Discord.Client({
+    intents: []
+});
+
+bot.on('ready', async() => {
+    console.log(`Connected`);
+    console.log(`Logged in as: `);
+    console.log(`${bot.user.tag}`);
+});
+
+bot.login(auth.token);
