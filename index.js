a726773b469831c1d6545f277654e9636e5d18ed
CloterChohan <CloterChohan@gmail.com>
2021-10-14 10:27:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-const Discord = require('discord.js');
 const auth = require('./auth.json');
-// Initialize Discord Bot
+const { Client, Intents } = require('discord.js');
 
-const bot = new Discord.Client({
-    intents: []
-});
+const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 bot.on('ready', async() => {
     console.log(`Connected`);
     console.log(`Logged in as: `);
     console.log(`${bot.user.tag}`);
 });
+//command
+let prefix = '!';
+
+bot.on("message", function(message) { 
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;                             
+    const commandBody = message.content.slice(prefix.length);
+    const args = commandBody.split(' ');
+    const command = args.shift().toLowerCase();
+
+    if(command === "servetea"){
+        const timeTaken = Date.now() - message.createdTimestamp;
+        message.reply(`FUCK YOU BITCH This message had a latency of ${timeTaken}ms.`);
+    }
+});  
+
 
 bot.login(auth.token);
